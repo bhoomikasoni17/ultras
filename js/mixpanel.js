131eeab528537a039b0a00d6be2be9c4b62e5032
bhoomikasoni17 <bhoomika.soni@dentsu.com>
2024-09-16 12:07:35 +0530

    Update mixpanel.js

--- js/mixpanel.js
+++ js/mixpanel.js
@@ -10,5 +10,6 @@ mixpanel.init("8c1bc97d7bc0b91017760e446f79e022", {
 
 //index.html shop it now button tracking
 function shopitnow(){
+  console.log("hi shop it now");
   mixpanel.track("Button",{"buttonname": "SHOP IT NOW"});
 }
